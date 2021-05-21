66b9df672edb37e895039af4df2b58acc3c6539b
cchitec <79786994+cchitec@users.noreply.github.com>
2021-05-21 10:05:14 -0600

    update

--- app.py
+++ app.py
@@ -6,3 +6,21 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 
 from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+session = Session(engine)
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    """Available api routes."""
